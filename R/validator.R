fdbfaf76a4c9b0ab9ca7f8353fec545ff7702498
Nowosad <tupiszakaczki@gmail.com>
2020-06-15 19:09:13 +0200

    splits validator into single_validator and validator

--- R/validator.R
+++ R/validator.R
@@ -1,7 +1,6 @@
 #' Title
 #'
 #' @param x
-#' @param parameter
 #' @param start_date
 #' @param end_date
 #'
@@ -9,8 +8,19 @@
 #' @export
 #'
 #' @examples
-validator = function(x, parameter, start_date = as.Date("2019-07-25"), end_date = as.Date("2019-07-30")){
-  x = cleaner(x) %>%
+validator = function(x, start_date = as.Date("2019-07-25"), end_date = as.Date("2019-07-30")){
+  x %>%
+    cleaner() %>%
+    dplyr::group_split(parameter) %>%
+    purrr::map(single_validator, start_date, end_date) %>%
+    purrr::map_df("error_df") %>%
+    dplyr::mutate(filename = basename(x)) %>%
+    dplyr::select(filename, dplyr::everything())
+}
+single_validator = function(x, start_date = as.Date("2019-07-25"), end_date = as.Date("2019-07-30")){
+  parameter = unique(x$parameter)
+
+  x = x %>%
     dplyr::filter(dplyr::between(as.Date(TIMESTAMP), start_date, end_date)) %>%
     dplyr::filter(parameter == !!parameter)
 
@@ -18,15 +28,18 @@ validator = function(x, parameter, start_date = as.Date("2019-07-25"), end_date 
     dplyr::filter(parameter == !!parameter) %>%
     dplyr::filter(site == unique(x$site))
 
-  if(nrow(param_df) == 0) stop("Parameter ", parameter, " does not exist.\nTry a different one.", call. = FALSE)
-
-
-  error_df_template = data.frame(TIMESTAMP = .POSIXct(character()),
+  error_df_template = data.frame(TIMESTAMP = x$TIMESTAMP[0],
                                  RECORD = numeric(),
                                  name = character(),
                                  value = numeric(),
                                  problem = character())
-  x = list(correct_df = x, error_df = x[0, ])
+  x = list(correct_df = x, error_df = error_df_template)
+
+  # if(nrow(param_df) == 0) stop("Parameter ", parameter, " does not exist.\nTry a different one.", call. = FALSE)
+  if(nrow(param_df) == 0) {
+    warning("Parameter ", parameter, " does not exist.\nTry a different one.", call. = FALSE)
+    return(x)
+  }
 
   x = validate_nan(x)
   x = validate_min(x, param_df = param_df)
